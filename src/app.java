c166315a7c1555d16105b0c4e53d936a4d05b663
mbistuer <m.bistuer.sallan@gmail.com>
2016-03-21 13:44:36 +0100

    Initial commit

---
+++ src/app.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+public class app {
+
+
+    public static void main(String[] args){
+        Frame marco=new Frame();
+        marco.setSize(1080,620);
+        marco.setTitle("Carrera de camellos");
+        marco.setVisible(true);
+
+
+
+try {
+    camello a1 = new camello("baloo", 300,"/Users/Mateo/Desktop/Sockets/camel.wav");
+    camello a2 = new camello("pistolo", 200,"/Users/Mateo/Desktop/Sockets/c.wav");
+    camello a3 = new camello("mateo", 400,"/Users/Mateo/Desktop/Sockets/c.wav");
+    a1.imagen(marco );
+    a2.imagen(marco );
+    a3.imagen(marco );
+
+
+    a1.start();
+    a2.start();
+    a3.start();
+}
+catch (Exception E){
+
+}
+
+    }
+}
---
+++ src/camello.java
@@ -0,0 +1,99 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import sun.audio.*;
+
+
+public class camello extends Thread {
+    private String nombre;
+    private int y ;
+    private    String gongFile ;
+    int total=100;
+    JLabel img = new JLabel(" ");
+
+
+
+    public camello(String nombre,int y,String song){
+        this.nombre=nombre;
+        this.y=y;
+    this.gongFile=song;
+
+
+
+    }
+
+
+
+    public void imagen(Frame a) throws Exception{
+
+
+
+
+
+
+
+        ImageIcon image = new ImageIcon("/Users/Mateo/Desktop/camello.gif");
+        img.setIcon(image);
+        img.setSize(135,135);
+        img.setLocation(total,y);
+        img.setText(nombre);
+        img.setVisible(true);
+        a.add(img);
+
+
+    }
+
+
+    public void run()
+    {
+
+        long es=0;
+
+
+        while(true){
+            try{
+                es=(long)(Math.random()*3+1)*100;
+
+
+                InputStream in = new FileInputStream(gongFile);
+
+                // create an audiostream from the inputstream
+                AudioStream audioStream = new AudioStream(in);
+
+                System.out.println(nombre+" corriendo a "+es+"segundos");
+                Thread.sleep(es);
+                int a = (int) es;
+
+                total=total+a/10;
+                img.setLocation(total,y);
+
+                AudioPlayer.player.start(audioStream);
+
+                if(total>900){
+
+                    break;
+
+
+                }
+
+            }
+            catch (Exception e){
+
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+
+        System.out.println(nombre+" ha pasado la meta");
+
+
+
+
+    }
+
+
+
+}
