1be13acdddf4dc4504440697bb244a24f0b6cfde
mbistuer <m.bistuer.sallan@gmail.com>
2016-03-21 13:51:04 +0100

    push

--- src/app.java
+++ src/app.java
@@ -15,9 +15,9 @@ try {
     camello a1 = new camello("baloo", 300,"/Users/Mateo/Desktop/Sockets/camel.wav");
     camello a2 = new camello("pistolo", 200,"/Users/Mateo/Desktop/Sockets/c.wav");
     camello a3 = new camello("mateo", 400,"/Users/Mateo/Desktop/Sockets/c.wav");
-    a1.imagen(marco );
-    a2.imagen(marco );
-    a3.imagen(marco );
+    a1.imagen(marco);
+    a2.imagen(marco);
+    a3.imagen(marco);
 
 
     a1.start();
